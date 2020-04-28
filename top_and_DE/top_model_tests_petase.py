1741a6b9b40f0c1752d36d9e6c558a5caa1c5767
Andrew Favor <andrew@Andrews-MacBook-Pro-2.local>
2020-04-28 11:55:10 -0700

    “eszrdxtfcygvuhb”

--- top_and_DE/top_model_tests_petase.py
+++ top_and_DE/top_model_tests_petase.py
@@ -25,17 +25,8 @@ from mpl_toolkits.mplot3d import Axes3D
 
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
-
-
-# from sklearn.inspection import partial_dependence
-# from sklearn.inspection import plot_partial_dependence
-# from sklearn.experimental import enable_hist_gradient_boosting  # noqa
-# from sklearn.ensemble import HistGradientBoostingRegressor
 from sklearn.neural_network import MLPRegressor
-# from sklearn.datasets import fetch_california_housing
-#################
-### FUNCTIONS ###
-#################
+
 
 
 def get_fasta_letters(filename): # given the fasta filename for the wild-type, turn it into a letter-representation of the amino acid sequence
@@ -69,87 +60,7 @@ def get_eUniRep(S): # this is just a placeholder function until we get the funct
 	return np.array(X)[:,0,:] # return the eUniRep sequence representation vector/vectors
 
 
-# function for introducing mutations to a given sequence
-def mutate_sequence(seq,m): # produce a mutant sequence (integer representation), given an initial sequence and the number of mutations to introduce ("m")
 
-	for i in range(m): #iterate through number of mutations to add
-
-		rand_loc = random.randint(132,212) # find random position to mutate
-		rand_aa = random.randint(1,23) # find random amino acid to mutate to
-		seq = list(seq)
-		seq[rand_loc] = get_int_to_aa()[rand_aa] # update sequence to have new amino acid at randomely chosen position
-		seq = ''.join(seq)
-
-
-	return seq # output the randomely mutated sequence
-
-
-# Directed-evolution function: 
-def directed_evolution(s_wt,num_iterations,T,Model): # input = (wild-type sequence, number of mutation iterations, "temperature")		
-
-
-	s_traj = [] # initialize an array to keep records of the protein sequences for this trajectory
-	y_traj = [] # initialize an array to keep records of the fitness scores for this trajectory
-
-
-	s = mutate_sequence(s_wt, (np.random.poisson(2) + 1)) # initial mutant sequence for this trajectory, with m = Poisson(2)+1 mutations
-	x = get_eUniRep(np.array([s])) # eUniRep representation of the initial mutant sequence for this trajectory
-	y = Model.predict(x) # predicted fitness score for the initial mutant sequence for this trajectory
-
-	for i in range(num_iterations): # iterate through the trial mutation steps for the directed evolution trajectory
-
-		mu = np.random.uniform(1,2.5) # "mu" parameter for poisson function: used to control how many mutations to introduce
-		m = np.random.poisson(mu-1) + 1 # how many random mutations to apply to current sequence
-
-		s_new = mutate_sequence(s, m) # new trial sequence, produced from "m" random mutations
-		x_new = get_eUniRep(np.array([s_new])) # new eUniRep representation for trial sequence
-		y_new = Model.predict(x_new) # new fitness value for trial sequence
-
-		p = min(1,np.exp((y_new-y)/T)) # probability function for trial sequence
-		if random.random() < p: # metropolis-Hastings update selection criteria
-			s, y = s_new, y_new # if criteria is met, update sequence and corresponding fitness
-
-		s_traj.append(s) # update the sequence trajectory records for this iteration of mutagenesis
-		y_traj.append(y) # update the fitness trajectory records for this iteration of mutagenesis
-
-
-	return s_traj, y_traj # output = (sequence record for trajectory, fitness score recorf for trajectory)
-
-
-def run_DE_trajectories(s_wt, X,Y, num_iterations, num_trajectories, Model):
-
-	X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-	Model.fit(X_train,Y_train)
-	T = 0.01 # "temperature": a mutagenesis parameter that controls rate of trial-mutation acceptance
-
-	s_records = []
-	y_records = []
-
-	for i in range(num_trajectories): #iterate through however many mutation trajectories we want to sample
-		s_traj, y_traj = directed_evolution(s_wt,num_iterations,T,Model) # call the directed evolution function, outputting the trajectory sequence and fitness score records
-
-		s_records.append(s_traj) # update the sequence trajectory records for this full mutagenesis trajectory
-		y_records.append(y_traj) # update the fitness trajectory records for this full mutagenesis trajectory
-
-	s_records = np.array(s_records)
-	y_records = np.array(y_records)
-
-	plt.plot(np.transpose(y_records[:,:,0])) # plot the changes in fitness for all sampled trajectories
-	plt.ylabel(r'$\Delta T_m$')
-	plt.xlabel('Mutation Steps')
-	plt.show() # cuz it is very satisfying to visualize this stuff :)
-
-	return s_records, y_records
-
-
-def shuffle(X,Y):
-	indices = np.random.permutation(np.shape(X)[0])
-	X,Y = X[indices], Y[indices]
-	return X,Y
-
-
-def calc_loss(Y_test,preds):
-	return np.linalg.norm(Y_test-preds)
 
 def validation_test(X,Y,Model):
 	X,Y = shuffle(X,Y)
